63c6685a6a32d3a1c9b68a42e823b0aed673d4da
laithaustin <laithaustin@utexas.edu>
2021-11-20 17:11:05 -0600

    implemented adding users and images

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,34 @@
+import React from "react";
 import './App.css';
+import Header from "./header";
+import AddContact from "./addContact";
+import ContactList from "./ContactList";
+import { useState, useEffect } from 'react';
 
 function App() {
-  return ( <div>Hello World!</div> )
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact)
+    setContacts([...contacts, contact]);
+  };
+
+  useEffect(() =>{
+    const gContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (gContacts) setContacts(gContacts);
+  }, [])
+
+  useEffect(() =>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  }, [contacts])
+
+  return ( <div className = "ui container">
+            <Header/>
+            <AddContact addContactHandler = {addContactHandler}/>
+            <ContactList contacts = {contacts}/>
+          </div>
+    );
     
 }
 
